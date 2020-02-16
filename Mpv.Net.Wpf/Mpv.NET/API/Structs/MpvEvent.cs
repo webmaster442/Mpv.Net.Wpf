e8b9c7489f0e9ac3da41d6299da3a68dddbafc29
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-02-16 19:29:20 +0100

    Added Mpv.NET source code

---
+++ Mpv.Net.Wpf/Mpv.NET/API/Structs/MpvEvent.cs
@@ -0,0 +1,26 @@
+﻿using Mpv.NET.API.Interop;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mpv.NET.API
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct MpvEvent
+	{
+		public MpvEventID ID;
+
+		public MpvError Error;
+
+		public ulong ReplyUserData;
+
+		public IntPtr Data;
+
+		public TData? MarshalDataToStruct<TData>() where TData : struct
+		{
+			if (Data == IntPtr.Zero)
+				return default;
+
+			return MpvMarshal.PtrToStructure<TData>(Data);
+		}
+	}
+}
